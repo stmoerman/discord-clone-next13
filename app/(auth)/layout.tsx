b36f67cbb4cf370e664dbb2e34631f13384979c6
Stephan Moerman <stephan.moerman@viewnary.com>
2023-08-27 12:49:49 +0200

    Finished integrating authentication using clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
 };
 
 export default AuthLayout;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
@@ -16,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
